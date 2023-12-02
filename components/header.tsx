a5beacf000d55fb9d0374138963583e7b5a9eaf0
Anders Malling privat <Andersmalling@gmail.com>
2023-12-02 20:32:19 +0100

    added sections + alot more

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,34 @@
 
 import React from "react";
 import { motion } from "framer-motion";
+import Link from "next/link";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
 
 export default function Header() {
   return (
@@ -11,6 +39,21 @@ export default function Header() {
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-10  sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative cursor-pointer"
+              key={link.hash}
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              href={link.hash}
+            >
+              {link.name}
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
