c1a08997d96547abb8744c8c5d77d22cc050eaf8
Anders Malling privat <Andersmalling@gmail.com>
2023-12-21 18:28:49 +0100

    added analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
 
         <Header />
         {children}
+        <Analytics />
       </body>
     </html>
   );
